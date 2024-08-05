d2a8843e0e0bf662b4f82093f283cad563eae278
Daria <149780226+dariaztva10@users.noreply.github.com>
2024-08-05 14:36:44 +0000

    Final

--- src/js/component/traffic_light.jsx
+++ src/js/component/traffic_light.jsx
@@ -1,34 +1,28 @@
 import React, { useState } from "react";
 
-
-
-
 const TrafficLight = () => {
     const [color, setColor] = useState("red");
+
+    const getRandomColor = () => {
+        const colors = ["red", "yellow", "green"];
+        const randomColor = colors[Math.floor(Math.random() * colors.length)];
+        setColor(randomColor);
+    };
+
     return (
         <div className="container">
-            <div className="semaforo col">
-
-                <button className={color === "red" ? "red glow": "red"} onClick={() => {
-                    setColor("red" 
-                    );
-                }}></button>
-
-
-                <button className={color === "yellow" ? "yellow glow": "yellow"} onClick={() => {
-                    setColor("yellow");
-                }}></button>
-
-
-                <button className={color === "green" ? "green glow": "green"} onClick={() => {
-                    setColor("green");
-                }}></button>
-
+            <div className="paloSemaforo"></div>
+            <div className="semaforo">
+                <button className={color === "red" ? "red glow" : "red"} onClick={() => setColor("red")}></button>
+                <button className={color === "yellow" ? "yellow glow" : "yellow"} onClick={() => setColor("yellow")}></button>
+                <button className={color === "green" ? "green glow" : "green"} onClick={() => setColor("green")}></button>
+                <button className={color === "especialMorado" ? "purple glow" : "especialMorado"} onClick={() => setColor("especialMorado")}>Morado</button>
             </div>
-            <div className="paloSemaforo col">
-
+            <div className="alternador">
+                <button className="btn btn-outline-success" onClick={getRandomColor}>Botón alternador</button>
             </div>
         </div>
     );
 };
+
 export default TrafficLight;
