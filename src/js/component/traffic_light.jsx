497e95de53c22a2638051f183359ae9fe93ee5bc
Daria <149780226+dariaztva10@users.noreply.github.com>
2024-08-07 07:25:44 +0000

    Final del proyecto

--- src/js/component/traffic_light.jsx
+++ src/js/component/traffic_light.jsx
@@ -2,11 +2,16 @@ import React, { useState } from "react";
 
 const TrafficLight = () => {
     const [color, setColor] = useState("red");
+    const [purple, setPurple] = useState(false);
 
     const getRandomColor = () => {
-        const colors = ["red", "yellow", "green"];
+        const colors = ["red", "yellow", "green", "purple",];
         const randomColor = colors[Math.floor(Math.random() * colors.length)];
         setColor(randomColor);
+    };
+
+    const getMorado = () => {
+        setPurple(!purple);
     };
 
     return (
@@ -16,11 +21,15 @@ const TrafficLight = () => {
                 <button className={color === "red" ? "red glow" : "red"} onClick={() => setColor("red")}></button>
                 <button className={color === "yellow" ? "yellow glow" : "yellow"} onClick={() => setColor("yellow")}></button>
                 <button className={color === "green" ? "green glow" : "green"} onClick={() => setColor("green")}></button>
-                <button className={color === "especialMorado" ? "purple glow" : "especialMorado"} onClick={() => setColor("especialMorado")}>Morado</button>
+                {purple && (
+                    <button className="purple"></button>
+                )}
             </div>
             <div className="alternador">
-                <button className="btn btn-outline-success" onClick={getRandomColor}>Botón alternador</button>
+                <button className="btn btn-success" onClick={getRandomColor}>Botón alternador</button>
             </div>
+            <button className="btn btn-success" onClick={(getMorado)}>Botón morado</button>
+            
         </div>
     );
 };
